0dd8e576b66f51af7b99e7e601679598fdb98dc0
Alec-Angus Macdonald <alecangus@gmail.com>
2018-02-18 19:02:17 +0000

    Added actual files... (hopefully)

---
+++ src/main/java/com/yalday/yaldayBoot/YaldayBootApplication.java
@@ -0,0 +1,12 @@
+package com.yalday.yaldayBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaldayBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaldayBootApplication.class, args);
+	}
+}
