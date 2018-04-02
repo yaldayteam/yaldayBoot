12822720f4a634c78579bcd926fcd9aac74c762d
nsawoky <azyk.git@gmail.com>
2018-04-02 23:03:25 +0100

    adding tests

---
+++ src/test/java/com/yalday/yaldayBoot/entity/MerchantTest.java
@@ -0,0 +1,11 @@
+package com.yalday.yaldayBoot.entity;
+
+
+import javax.persistence.*;
+
+/**
+ * Created by Home on 25/03/2018.
+ */
+public class MerchantTest {
+
+}
---
+++ src/test/java/com/yalday/yaldayBoot/exception/ResourceNotFoundExceptionTest.java
@@ -0,0 +1,7 @@
+package com.yalday.yaldayBoot.exception;
+
+/**
+ * Created by Home on 25/03/2018.
+ */
+public class ResourceNotFoundExceptionTest {
+}
