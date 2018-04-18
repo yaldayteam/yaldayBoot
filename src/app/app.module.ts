c26da0bea87ebcafa77c5ca08e7fcf2b576e9236
chriskiernan87 <chriskiernan87@googlemail.com>
2018-04-18 19:07:29 +0100

    Adding some basic from end merchant details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { MerchantsComponent } from './merchants/merchants.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MerchantsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/merchant.ts
@@ -0,0 +1,4 @@
+export class Merchant{
+  id: number;
+  name: string;
+}
